69f648c7586a8fb0a5f812770421b65b08ca98e2
Anthony <a_manongsong@hotmail.com>
2018-07-04 22:07:32 -0400

    Update server.go

--- serverSrc/server.go
+++ serverSrc/server.go
@@ -5,12 +5,16 @@ import(
 	"net/http"
 )
 
-
 func main() {
 	fmt.Println("Starting Server...")
+	
+	//Setup server to handle static files 
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("static"))))
+	
 	fmt.Println("Start Successful...")
 	fmt.Println("Listening for connections on port 8080")
 	fmt.Println("Server now Active...")
+	
+	//Configure listener on port 8080
 	http.ListenAndServe(":8080",nil)
 }
